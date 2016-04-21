b283b276e93f9a08f46e1e1ca9af6f401014db3b
BitScout <pixel1983@gmail.com>
2016-04-21 22:07:49 +0200

    Added Drums.ino

---
+++ Drums.ino
@@ -0,0 +1,36 @@
+
+#define LED1 0
+#define LED2 4
+
+void setup() {
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+}
+
+void loop() {
+
+  delay(350);
+
+  beat(LED1);
+  beat(LED2);
+  beat(LED1);
+  beat(LED2);
+}
+
+void beat(int led) {
+  int fadeDelay = 1;
+  int fadeStep = 4;
+  
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += fadeStep) {
+    analogWrite(led, fadeValue);
+    delay(fadeDelay);
+  }
+  
+  delay(random(60,70));
+  
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= fadeStep) {
+    analogWrite(led, fadeValue);
+    delay(fadeDelay);
+  }
+}
+
